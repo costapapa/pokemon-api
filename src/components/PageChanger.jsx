7a7399b99cbe24a8f05f5387986b117fb88bf4f3
costapapa <cost.papa87@gmail.com>
2023-09-21 12:00:07 +0100

    pokemon api learning

---
+++ src/components/PageChanger.jsx
@@ -0,0 +1,8 @@
+export default function PageChanger({ gotoNextPage, gotoPrevPage }) {
+  return (
+    <div>
+      {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
+      {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+    </div>
+  );
+}
